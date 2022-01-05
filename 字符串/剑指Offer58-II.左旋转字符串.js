5c3c014a0577be195866e6e735a91a04ce1becfb
Ean <starryeve@qq.com>
2022-01-05 21:34:48 +0800

    剑指Offer58-II.左旋转字符串,完全翻转+局部翻转

--- 字符串/剑指Offer58-II.左旋转字符串.js
+++ 字符串/剑指Offer58-II.左旋转字符串.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zengfh
  * @Date: 2022-01-05 21:17:04
- * @LastEditTime: 2022-01-05 21:19:34
+ * @LastEditTime: 2022-01-05 21:33:47
  * @Description: 
  */
 /**
@@ -9,15 +9,38 @@
  * @param {number} n
  * @return {string}
  */
+// var reverseLeftWords = function(s, n) {
+//   const arr = Array.from(s);
+
+//   const left = arr.splice(0, n);
+
+//   arr.push(...left);
+
+//   return arr.join("");
+// }
+
+/**
+ * 先整体翻转，再局部翻转
+ * @param {*} s
+ * @param {*} n
+ * @return {*}
+ */
 var reverseLeftWords = function(s, n) {
   const arr = Array.from(s);
 
-  const left = arr.splice(0, n);
-
-  arr.push(...left);
+  reverse(arr, 0, arr.length - 1);
+  reverse(arr, 0, arr.length - n - 1);
+  reverse(arr, arr.length - n, arr.length - 1);
 
   return arr.join("");
 }
 
+const reverse = function(arr, start, end) {
+  while(start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start ++;
+    end --;
+  }
+}
 const s = "abcdefg", k = 2
 console.log(reverseLeftWords(s, 2));
