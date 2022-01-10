4d3d46287002d35720bd256b6b19a95a73431be5
Ean <starryeve@qq.com>
2022-01-10 14:54:36 +0800

    232. 用栈实现队列

---
+++ 栈和队列/232. 用栈实现队列.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: zengfh
+ * @Date: 2022-01-10 14:47:29
+ * @LastEditTime: 2022-01-10 14:51:53
+ * @Description: 
+ */
+const MyQueue = function() {
+  this.stack1 = [];
+  this.stack2 = [];
+}
+
+MyQueue.prototype.push = function(x) {
+  this.stack1.push(x);
+}
+
+MyQueue.prototype.pop = function(x) {
+  if(this.stack2.length > 0) return this.stack2.pop();
+
+  while(this.stack1.length > 0) {
+    this.stack2.push(this.stack1.pop());
+  }
+
+  return this.stack2.pop();
+}
+
+MyQueue.prototype.top = function() {
+  const top = this.pop();
+  this.stack2.push(top);
+
+  return top;
+}
+
+MyQueue.prototype.empty = function() {
+  return this.stack1.length || this.stack2.length;
+}
