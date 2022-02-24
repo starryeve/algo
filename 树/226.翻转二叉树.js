c8457d768471be611acdf0667e066ce4c73c880c
Ean <starryeve@qq.com>
2022-02-24 08:47:27 +0800

    226. 翻转二叉树

--- 树/226.翻转二叉树.js
+++ 树/226.翻转二叉树.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zengfh
  * @Date: 2022-01-11 20:57:09
- * @LastEditTime: 2022-01-11 21:01:01
+ * @LastEditTime: 2022-02-22 09:39:18
  * @Description: 
  */
  function TreeNode(val, left, right) {
@@ -14,20 +14,36 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
- var invertTree = function(root) {
-  const invertNode = function(root) {
-    if(!root) return;
+//  var invertTree = function(root) {
+//   const invertNode = function(root) {
+//     if(!root) return;
 
-    let temp = root.left;
-    root.left = root.right;
-    root.right = root.left;
+//     let temp = root.left;
+//     root.left = root.right;
+//     root.right = root.left;
+//   }
+
+//   if(root === null) return root;
+
+//   invertNode(root);
+//   invertTree(root.left);
+//   invertTree(root.right);
+
+//   return root;
+// };
+
+const invertTree = function(root) {
+  const invertNode = function(node1, node2) {
+    let temp = node1;
+    node1 = node2;
+    node2 = temp;
   }
 
-  if(root === null) return root;
-
-  invertNode(root);
+  if(root === null) return;
+  invertNode(root.left, root.right);
   invertTree(root.left);
   invertTree(root.right);
 
+
   return root;
-};
+}
