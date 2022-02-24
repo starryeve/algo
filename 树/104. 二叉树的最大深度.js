27cb580691990a29947927072876d15f3debc7ab
Ean <starryeve@qq.com>
2022-02-24 08:55:42 +0800

    104. 二叉树的最大深度,前序回溯求深度

--- 树/104. 二叉树的最大深度.js
+++ 树/104. 二叉树的最大深度.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zengfh
  * @Date: 2022-02-24 08:18:58
- * @LastEditTime: 2022-02-24 08:39:05
+ * @LastEditTime: 2022-02-24 08:53:33
  * @Description: 
  */
 
@@ -14,18 +14,43 @@ function TreeNode(val, left, right) {
  * @param {TreeNode} root
  * @return {number}
  */
- var maxDepth = function(root) {
-  // 1. 确定递归参数，只需要传入树的根节点root
+//  var maxDepth = function(root) {
+//   // 1. 确定递归参数，只需要传入树的根节点root
 
-  // 2. 确定递归终止条件
+//   // 2. 确定递归终止条件
 
-  if(root === null) return 0
+//   if(root === null) return 0
 
-  // 3. 编写单层递归逻辑
-  let leftDepth = maxDepth(root.left)
-  let rightDepth = maxDepth(root.right)
-  return 1 + (leftDepth > rightDepth ?  leftDepth : rightDepth)
-};
+//   // 3. 编写单层递归逻辑
+//   let leftDepth = maxDepth(root.left)
+//   let rightDepth = maxDepth(root.right)
+//   return 1 + (leftDepth > rightDepth ?  leftDepth : rightDepth)
+// };
+
+var maxDepth = function(root) {
+  let res = 0
+  function getDepth(root, depth) {
+    res = depth > res ? depth : res
+
+    if(root.left === null && root.right === null) return
+
+    if(root.left) {
+      depth ++
+      getDepth(root.left, depth)
+      depth --
+    }
+    if(root.right) {
+      depth ++
+      getDepth(root.right, depth)
+      depth --
+    }
+    return 
+  }
+
+  if(root === null) return res
+  getDepth(root, 1)
+  return res
+}
 
 const tree = new TreeNode(1,
   new TreeNode(2,
--- 树/层序遍历.js
+++ 树/层序遍历.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zengfh
  * @Date: 2022-01-11 20:38:59
- * @LastEditTime: 2022-01-11 20:52:28
+ * @LastEditTime: 2022-02-22 08:49:34
  * @Description: 
  */
 
@@ -11,6 +11,34 @@ const TreeNode = function(val, left, right) {
   this.right = right === undefined ? null : right;
 }
 
+// const levelOrderTraversal = function(root) {
+//   const res = [];
+//   const queue = [];
+
+//   if(root) {
+//     queue.push(root);
+//   }
+
+//   while(queue.length) {
+//     const size = queue.length; // 本层的节点数
+//     const levelRes = []; // 本次的节点值
+//     for (let i = 0; i < size; i++) {
+//       const front = queue.shift();
+//       levelRes.push(front.val);
+//       if(front.left) { // 如果有左子树
+//         queue.push(front.left);
+//       }
+//       if(front.right) { // 如果有右子树
+//         queue.push(front.right);
+//       }
+//     }
+
+//     res.push(levelRes);
+//   }
+
+//   return res;
+// }
+
 const levelOrderTraversal = function(root) {
   const res = [];
   const queue = [];
@@ -19,20 +47,30 @@ const levelOrderTraversal = function(root) {
     queue.push(root);
   }
 
-  while(queue.length) {
-    const size = queue.length; // 本层的节点数
-    const levelRes = []; // 本次的节点值
-    for (let i = 0; i < size; i++) {
-      const front = queue.shift();
-      levelRes.push(front.val);
-      if(front.left) { // 如果有左子树
-        queue.push(front.left);
-      }
-      if(front.right) { // 如果有右子树
-        queue.push(front.right);
-      }
+  while(queue.length > 0) {
+    let size = queue.length; // 当前层节点数
+    while(size --) {
+      const node = queue.shift(); // 节点出队
+      res.push(node.val);
+      if(node.left) queue.push(node.left)
+      if(node.right) queue.push(node.right);
     }
-
-    res.push(levelRes);
   }
+
+
+  return res
 }
+
+const tree = new TreeNode(1,
+  new TreeNode(2,
+    new TreeNode(4, null, null),
+    null
+  ),
+  new TreeNode(3,
+    new TreeNode(5, null, null),
+    null
+  )
+)
+
+
+console.log(levelOrderTraversal(tree))
