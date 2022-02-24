92f06469abf2ee19a8f817528615d7d9d3babd5e
Ean <starryeve@qq.com>
2022-02-24 09:37:34 +0800

    222.完全二叉树的节点个数

---
+++ 树/222.完全二叉树的节点个数.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: zengfh
+ * @Date: 2022-02-24 09:19:05
+ * @LastEditTime: 2022-02-24 09:23:10
+ * @Description: 
+ */
+
+var countNodes = function(root) {
+  // 1. 确定递归参数和返回值，传入一棵树的根节点，返回该树的节点数
+
+  // 2. 确定递归终止条件
+  if(root === null) return 0
+
+  // 3. 编写单层递归逻辑
+  let leftNum = countNodes(root.left)
+  let rightNum = countNodes(root.right)
+
+  return 1 + leftNum + rightNum
+}
